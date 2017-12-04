6119337a41ff2f87e934615429c5ed2f9cc7e75c
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-12-04 13:11:45 -0800

    Completed Class based inheritance

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  
+  constructor () {
+  	super();			// if we dont give any parameters here, would it inherit everything and then we can just overwrite what we want ?
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
+
+
 };
